d3c566b9ba0c56d498a1af87af4806cdaa389c9a
luoxn28 <luoxn28@163.com>
2017-03-08 23:14:31 +0800

    add type handler registry

--- src/main/java/com/intrack/type/TypeHandlerRegistry.java
+++ src/main/java/com/intrack/type/TypeHandlerRegistry.java
@@ -1,16 +1,70 @@
 package com.intrack.type;
 
+import com.sun.deploy.security.ValidationState;
+
 import java.lang.reflect.Type;
-import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * TypeHandler registry, save all type handler
+ *
  * @author intrack
  */
 public class TypeHandlerRegistry {
 
-    
+    /**
+     *      type + type handler
+     * jdbc type + type handler
+     */
+    private final Map<Type, TypeHandler<?>> TYPE_HANDLER_MAP = new ConcurrentHashMap<>();
+    private final Map<JdbcType, TypeHandler<?>> JDBCTYPE_HANDLER_MAP = new ConcurrentHashMap<>();
+    //private final Map<Type, Map<JdbcType, TypeHandler<?>>> TYPE_HANDLER_MAP = new ConcurrentHashMap<>();
 
+    /* add all type handler */
+    {
+        register(Byte.class, JdbcType.ARRAY, new ByteTypeHandler());
+        register(Character.class, JdbcType.CHAR, new CharacterTypeHandler());
+        register(Double.class, JdbcType.DOUBLE, new DoubleTypeHandler());
+        register(Float.class, JdbcType.FLOAT, new FloatTypeHandler());
+        register(Integer.class, JdbcType.INTEGER, new IntegerTypeHandler());
+        register(Long.class, JdbcType.BIGINT, new LongTypeHandler());
+        register(Object.class, JdbcType.JAVA_OBJECT, new ObjectTypeHandler());
+        register(Short.class, JdbcType.SMALLINT, new ShortTypeHandler());
+        register(String.class, JdbcType.VARCHAR, new StringTypeHandler());
+    }
+
+    public TypeHandler<?> getTypeHandler(Type type) {
+        TypeHandler<?> typeHandler = null;
+
+        typeHandler = TYPE_HANDLER_MAP.get(type);
+        if (typeHandler == null) {
+            throw new TypeException("type handler is null.");
+        }
+
+        return typeHandler;
+    }
+
+    public TypeHandler<?> getTypeHandler(JdbcType jdbcType) {
+        TypeHandler<?> typeHandler = null;
+
+        typeHandler = JDBCTYPE_HANDLER_MAP.get(jdbcType);
+        if (typeHandler == null) {
+            throw new TypeException("type handler is null.");
+        }
+
+        return typeHandler;
+    }
+
+    // private scope
+
+    private <T> void register(Class clazz, JdbcType jdbcType, TypeHandler<T> typeHandler) {
+        register((Type) clazz, jdbcType, typeHandler);
+    }
+
+    private <T> void register(Type type, JdbcType jdbcType, TypeHandler<T> typeHandler) {
+        TYPE_HANDLER_MAP.put(type, typeHandler);
+        JDBCTYPE_HANDLER_MAP.put(jdbcType, typeHandler);
+    }
 }
--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,4 +1,10 @@
+import com.intrack.type.JdbcType;
+import com.intrack.type.TypeHandler;
+import com.intrack.type.TypeHandlerRegistry;
+
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * for test
@@ -16,8 +22,24 @@ public class TestMain {
         Statement statement = (Statement) connection.createStatement();
         ResultSet resultSet = statement.executeQuery("select * from users");
 
+        /* Get resultSet metadata */
+        ResultSetMetaData metaData = resultSet.getMetaData();
+        List<TypeHandler<?>> typeHandlers = new ArrayList<>();
+        int column = metaData.getColumnCount();
+
+        TypeHandlerRegistry typeHandlerRegistry = new TypeHandlerRegistry();
+        for (int i = 1; i <= column; i++) {
+            JdbcType jdbcType = JdbcType.forCode(metaData.getColumnType(i));
+            typeHandlers.add(typeHandlerRegistry.getTypeHandler(jdbcType));
+        }
         while (resultSet.next()) {
-            System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2));
+            //System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2));
+
+            for (int i = 1; i <= typeHandlers.size(); i++) {
+                TypeHandler typeHandler = typeHandlers.get(i - 1);
+                System.out.println(typeHandler.getResult(resultSet, i));
+            }
+            System.out.println("---------------------");
         }
     }
 }
