89ce22855884b021ca56dc565583c85aeac582e2
luoxn28 <luoxn28@163.com>
2017-03-13 22:51:13 +0800

    add DataSource

--- src/main/java/com/intrack/executor/DefaultExecutor.java
+++ src/main/java/com/intrack/executor/DefaultExecutor.java
@@ -5,8 +5,10 @@ import com.intrack.executor.resultset.ResultSetHandler;
 import com.intrack.executor.resultset.ResultSetWrapper;
 import com.intrack.executor.statement.DefaultStatementHandler;
 import com.intrack.executor.statement.StatementHandler;
+import com.intrack.mapping.Environment;
 import com.intrack.session.Configuration;
 import com.intrack.test.User;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -19,8 +21,10 @@ import java.util.List;
  */
 public class DefaultExecutor implements Executor {
 
-    private Configuration  configuration;
+    private Configuration configuration;
     private StatementHandler statementHandler = new DefaultStatementHandler();
+
+    private BasicDataSource dataSource = new BasicDataSource();
 
     public DefaultExecutor(Configuration configuration) {
         this.configuration = configuration;
@@ -28,21 +32,17 @@ public class DefaultExecutor implements Executor {
 
     @Override
     public <E> List<E> query(String statementSql) {
-        /* 加载sql驱动 */
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        /* 设置DataSource*/
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://192.168.1.150/ssh_study");
+        dataSource.setUsername("luoxn28");
+        dataSource.setPassword("123456");
 
-        String url = "jdbc:mysql://192.168.1.150/ssh_study";
-        String username = "luoxn28";
-        String password = "123456";
         Connection connection = null;
         PreparedStatement preparedStatement = null;
 
         try {
-            connection = DriverManager.getConnection(url, username, password);
+            connection = dataSource.getConnection();
             preparedStatement = connection.prepareStatement(statementHandler.prepare(statementSql, null));
             preparedStatement.executeQuery();
         } catch (SQLException e) {
